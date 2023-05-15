9f21c38049a15b41f54f57c0623f575225ee2d66
yusuf <kingshakzy@gmail.com>
2023-05-15 15:58:00 +0100

    Backend need to fix their shit

--- src/pages/signupuser.jsx
+++ src/pages/signupuser.jsx
@@ -27,8 +27,8 @@ export const SignUpUser  = () =>{
     })
     const submitData = (data) =>{
         console.log(data);
-        let newUser = JSON.stringify({ type: "regular", first_name: "Fortune", last_name: "Ishaku", email: data.email, username: data.username, password: data.password, password2: data.confirmPassword });
-        client.post("/auth/register/", newUser).then((res) => console.log(res.error)).catch((err) => console.error(err))
+
+        client.post("/auth/register/", JSON.stringify({ type: "regular", first_name: "Fortune", last_name: "Ishaku", email: data.email, username: data.username, password: data.password, password2: data.confirmPassword })).then((response) => console.log(response.data)).catch((error) => console.error(error))
     };
     return (
         <>
