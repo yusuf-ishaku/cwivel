acf446d5a72d3f9480a54f7be613ae444e8e97d7
yusuf <kingshakzy@gmail.com>
2023-05-21 20:30:06 +0100

    Worked on some flows

--- src/assets/components/navbar.jsx
+++ src/assets/components/navbar.jsx
@@ -4,11 +4,11 @@ export const Navbar = (props) => {
         <div className="w-full bg-cwivel-green px-4 md:px-14 flex py-4 flex-row justify-between items-center">
             <div className="text-white m-1 px-6 text-xs sm:text-lg block p-2">Cwivel<span className="text-teal-600">.com</span></div>
             <div className="hidden w-[50%]"><input type="search" name="Search" id="search" placeholder="Search"  className="placeholder-current w-full p-3 bg-white text-cwivel placeholder-black rounded-md focus:outline-none" /></div>
-            <div className="w-full sm:flex hidden ml-auto">
+            <div className="w-full sm:flex flex ml-auto">
                 <div className="w-fit flex flex-row items-center ml-auto">
                     <Link to="/"  className="text-xs activepage lg:mr-6 font-medium sm:text-xl block sm:p-2">Home</Link>
                     <Link to="/bookings" className={props.cl ? " text-xs lg:mr-6 text-gray-200 sm:text-base block p-2" : "hidden text-xs lg:mr-6 text-gray-200 sm:text-base p-2"}>Explore</Link>
-                    <Link to="/signupuser" className="text-xs text-gray-200 sm:text-base block p-2">Explore</Link>
+                    <Link to="/signupuser" className="text-xs text-gray-200 sm:text-base block p-2">Sign Up</Link>
                 </div>
                 <div className={props.cl ? "w-fit flex items-center ml-auto" : "w-fit hidden items-center ml-auto"}>
                     <Link to="/login" className="text-xs sm:text-lg text-white p-2">Login</Link>
